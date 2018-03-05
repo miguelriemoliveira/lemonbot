61364c3723e0f622c96ece5a40bd6e9cd46348f0
bernardomig <bernardomiguelourenco@gmail.com>
2018-03-05 18:18:54 +0000

    first point cloud is done

--- lemonbot_acquisition/src/lemonbot_acquisition_node.cpp
+++ lemonbot_acquisition/src/lemonbot_acquisition_node.cpp
@@ -13,17 +13,90 @@
 
 #include <flir_pantilt_d46/PtuGotoAction.h>
 
-int main()
-{
-  sensor_msgs::LaserScan ls;
-  for (float r : { 0.0, 0.1, 0.2, 0.3, 0.4, 0.5 })
-  {
-    ls.ranges.push_back(r);
-    ls.intensities.push_back(1.0);
-  }
-  sensor_msgs::PointCloud pc;
-  laser_geometry::LaserProjection _projector;
-  _projector.projectLaser(ls, pc);
+#include <laser_assembler/AssembleScans.h>
+#include <laser_assembler/AssembleScans2.h>
 
-  std::cout << pc.points.size() << std::endl;
+using namespace std;
+using namespace std::literals;
+
+using namespace laser_assembler;
+
+struct options
+{
+  string ptu = "/SetPTUState";
+  float min = -M_PI / 2;
+  float max = M_PI / 2;
+  float nsteps = 100;
+  float vel = 0.5f;
+} options;
+
+int main(int argc, char* argv[])
+{
+  // Initialize ROS connections
+  ros::init(argc, argv, "lemonbot_acquisition_node");
+
+  auto nh = ros::NodeHandle{};
+
+  // Create actionlib client for ptu
+  actionlib::SimpleActionClient<flir_pantilt_d46::PtuGotoAction> act_client(options.ptu, true);
+
+  ROS_INFO("started ptu action client at %s", options.ptu.c_str());
+
+  // wait for server to acknoledge
+  act_client.waitForServer();
+
+  ros::service::waitForService("assemble_scans");
+
+  ros::ServiceClient client = nh.serviceClient<AssembleScans2>("assemble_scans2");
+
+  AssembleScans2 srv;
+
+  ROS_INFO("connected!");
+
+  // create a new goal
+  flir_pantilt_d46::PtuGotoGoal goal;
+  goal.pan = -90;
+  goal.tilt = 0.0f;
+  goal.pan_vel = 10.0f;
+  goal.tilt_vel = 10.0f;
+
+  options.nsteps--;
+
+  float delta = (options.max - options.min) / (options.nsteps);
+
+  act_client.sendGoalAndWait(goal);
+
+  srv.request.begin = ros::Time::now();
+
+  // for (int step = 0; step <= (options.nsteps); ++step)
+  // {
+  //   float position = options.min + delta * step;
+  //   position *= 180 / M_PI;
+  //   ROS_INFO("goint to position %f", position);
+
+  //   goal.pan = position;
+
+  //   act_client.sendGoal(goal);
+
+  //   auto done = act_client.waitForResult(ros::Duration(30.0));
+  // }
+
+  goal.pan = +90;
+  act_client.sendGoalAndWait(goal);
+
+  srv.request.end = ros::Time::now();
+
+  client.call(srv);
+
+  auto pub = nh.advertise<decltype(srv.response.cloud)>("result", 10);
+
+  auto r = ros::Rate{ 10 };
+
+  while (ros::ok())
+  {
+    pub.publish(srv.response.cloud);
+    r.sleep();
+  }
+
+  return 0;
 }
