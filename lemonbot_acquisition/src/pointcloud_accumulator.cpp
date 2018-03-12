5e2ad17c279f8df9d42d6767f74fc187b415f090
bernardomig <bernardomiguelourenco@gmail.com>
2018-03-12 10:54:23 +0000

    merged wait_for_done and accumulator node. close #12

--- lemonbot_acquisition/src/pointcloud_accumulator.cpp
+++ lemonbot_acquisition/src/pointcloud_accumulator.cpp
@@ -1,7 +1,10 @@
 #include <lemonbot_acquisition/pointcloud_accumulator.h>
 
-PointcloudAccumulator::PointcloudAccumulator(std::string input, std::string output)
+PointcloudAccumulator::PointcloudAccumulator(std::string input, std::string output, std::string done_topic,
+                                             std::string result_topic)
   : Transformer(input, output, std::bind(&PointcloudAccumulator::accumulate, this, std::placeholders::_1))
+  , _done_sub(_nh.subscribe<std_msgs::Bool>(done_topic, 1, &PointcloudAccumulator::done, this))
+  , _result_pub(_nh.advertise<PointCloud>(result_topic, 1))
 {
 }
 
@@ -12,11 +15,18 @@ PointCloud PointcloudAccumulator::accumulate(const PointCloud& pc)
   return _accumulated;
 }
 
+void PointcloudAccumulator::done(const std_msgs::Bool::ConstPtr&)
+{
+  _result_pub.publish(_accumulated);
+
+  _accumulated.points.clear();
+}
+
 int main(int argc, char* argv[])
 {
   ros::init(argc, argv, "lemonbot_accumulator");
 
-  PointcloudAccumulator accumulator("input", "output");
+  PointcloudAccumulator accumulator("input", "output", "done", "result");
 
   ros::spin();
 
